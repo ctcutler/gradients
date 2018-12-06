88ab5ce234fc5f4cb91df9b29f89cbaf77e86488
ctcutler <ctcutler@gmail.com>
2018-12-05 22:02:54 -0500

    arbitrary scale func for gradient

--- gradients.py
+++ gradients.py
@@ -11,16 +11,18 @@ def init_solid(width, height, rgb):
 
     return np.reshape(tiled, (height, width, 3))
 
-def gradient(a, rgb):
+def gradient(a, rgb, scale_func):
     """
     Calculate deltas and add them to all points in a.
 
-    delta = cur_step/(step_count - 1) * (end_color - start_color)
+    delta = scale_func(cur_step, step_count) * (new_color - existing_color)
     """
+
+    (height, width, depth) = a.shape
     negated_a = a.copy() * -1
     color_deltas = negated_a.reshape(-1, 3) + rgb
-    scale_factors = np.arange(a.shape[0]) / (a.shape[0] - 1)
-    scaling = np.repeat(scale_factors, a.shape[1] * a.shape[2])
+    scale_factors = np.array([scale_func(x, height) for x in range(height)])
+    scaling = np.repeat(scale_factors, width * depth)
     scaled_deltas = scaling.reshape(a.shape) * color_deltas.reshape(a.shape)
 
     return a + scaled_deltas
@@ -30,10 +32,16 @@ def save_image(file_name, image_data):
     img.save(file_name)
 
 def main():
+    def linear_scale(cur, total):
+        return float(cur) / (total - 1)
+
+    def quadratic_scale(cur, total):
+        return float(cur**2) / ((total - 1)**2)
+
     solid = init_solid(WIDTH, HEIGHT, [0, 255, 0])
     save_image('solid.png', solid)
 
-    g = gradient(solid, [0, 0, 255])
+    g = gradient(solid, [0, 0, 255], quadratic_scale)
     save_image('gradient.png', g)
 
 if __name__== "__main__":
--- test_gradients.py
+++ test_gradients.py
@@ -13,12 +13,26 @@ class GradientsTest(unittest.TestCase):
         ]).reshape((2, 3, 3))
         np.testing.assert_equal(result, expected)
 
-    def test_gradient(self):
+    def test_linear_gradient(self):
+        def linear(cur, total):
+            return float(cur) / (total - 1)
         solid = gradients.init_solid(2, 3, (0, 0, 255))
-        result = gradients.gradient(solid, (0, 255, 0))
+        result = gradients.gradient(solid, (0, 255, 0), linear)
         expected = np.array([
             0, 0, 255, 0, 0, 255,
             0, 127.5, 127.5, 0, 127.5, 127.5,
+            0, 255, 0, 0, 255, 0,
+        ]).reshape((3, 2, 3))
+        np.testing.assert_equal(result, expected)
+
+    def test_quadratic_gradient(self):
+        def quadratic(cur, total):
+            return float(cur**2) / ((total - 1)**2)
+        solid = gradients.init_solid(2, 3, (0, 0, 255))
+        result = gradients.gradient(solid, (0, 255, 0), quadratic)
+        expected = np.array([
+            0, 0, 255, 0, 0, 255,
+            0, 63.75, 191.25, 0, 63.75, 191.25,
             0, 255, 0, 0, 255, 0,
         ]).reshape((3, 2, 3))
         np.testing.assert_equal(result, expected)
