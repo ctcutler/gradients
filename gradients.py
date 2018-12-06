818764e9b054e842f0cf4ac541ff17b790231408
ctcutler <ctcutler@gmail.com>
2018-12-05 20:38:31 -0500

    refactor gradient(), etc.

--- gradients.py
+++ gradients.py
@@ -8,50 +8,34 @@ HEIGHT = int(9 * 100 * .6)
 def init_solid(width, height, rgb):
     a = np.array(rgb)
     tiled = np.tile(a, width * height)
-    return np.reshape(tiled, (height, width, 3))
 
-def init_gradient(width, height, rgb1, rgb2, gradient_func):
-    a = np.empty((height, width, 3))
-    # FIXME: refactor based on gradient() approach?
-    for i in range(3):
-        gradient = gradient_func(rgb1[i], rgb2[i], height)
-        repeated = np.repeat(gradient, width)
-        shaped = np.reshape(repeated, (height, width))
-        a[:,:,i] = shaped
-    return a
+    return np.reshape(tiled, (height, width, 3))
 
 def gradient(a, rgb):
     """
-    for now:
-    - direction is always down
-    - rate of change is always linear
+    Calculate deltas and add them to all points in a.
 
-    delta = cur step/total steps * (end - start)
-
-    create a delta array and add it to the existing one. . . to do that I would need to:
-    - make a copy of the array
-        - multiply everything by -1
-        - add the end value to everything
-    - make a new array with the same size/shape
-        - fill every row with its index/(height-1)
-    - multiply these arrays together and add result to the original array
+    delta = cur_step/(step_count - 1) * (end_color - start_color)
     """
-    # FIXME: give this a good scrubbing
-    negated = np.copy(a) * -1
-    color_triples = negated.reshape(-1, 3)
-    deltas_flat = color_triples + rgb
-    deltas = np.reshape(deltas_flat, a.shape)
+    negated_a = a.copy() * -1
+    color_deltas = negated_a.reshape(-1, 3) + rgb
     scale_factors = np.arange(a.shape[0]) / (a.shape[0] - 1)
-    scale_factors_flat = np.repeat(scale_factors, a.shape[1] * a.shape[2])
-    scale_factors_reshaped = np.reshape(scale_factors_flat, a.shape)
-    return a + (scale_factors_reshaped * deltas)
+    scaling = np.repeat(scale_factors, a.shape[1] * a.shape[2])
+    scaled_deltas = scaling.reshape(a.shape) * color_deltas.reshape(a.shape)
+
+    return a + scaled_deltas
 
 def save_image(file_name, image_data):
     img = Image.fromarray(np.uint8(image_data), 'RGB')
     img.save(file_name)
 
-solid = init_solid(WIDTH, HEIGHT, [0, 255, 0])
-save_image('solid.png', solid)
+def main():
+    solid = init_solid(WIDTH, HEIGHT, [0, 255, 0])
+    save_image('solid.png', solid)
 
-g = gradient(solid, [0, 0, 255])
-save_image('gradient.png', g)
+    g = gradient(solid, [0, 0, 255])
+    save_image('gradient.png', g)
+
+if __name__== "__main__":
+    main()
+
--- test_gradients.py
+++ test_gradients.py
@@ -13,17 +13,6 @@ class GradientsTest(unittest.TestCase):
         ]).reshape((2, 3, 3))
         np.testing.assert_equal(result, expected)
 
-    def test_init_gradient(self):
-        result = gradients.init_gradient(
-            2, 3, (0, 0, 255), (0, 255, 0), np.linspace
-        )
-        expected = np.array([
-            0, 0, 255, 0, 0, 255,
-            0, 127.5, 127.5, 0, 127.5, 127.5,
-            0, 255, 0, 0, 255, 0,
-        ]).reshape((3, 2, 3))
-        np.testing.assert_equal(result, expected)
-
     def test_gradient(self):
         solid = gradients.init_solid(2, 3, (0, 0, 255))
         result = gradients.gradient(solid, (0, 255, 0))
