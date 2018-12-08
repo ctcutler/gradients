284b0ff5d3f3ae6192c0c382b3901857a9b4fa39
ctcutler <ctcutler@gmail.com>
2018-12-08 18:02:26 -0500

    update tests for scaling array approach

--- test_gradients.py
+++ test_gradients.py
@@ -14,10 +14,14 @@ class GradientsTest(unittest.TestCase):
         np.testing.assert_equal(result, expected)
 
     def test_linear_gradient(self):
-        def linear(cur, total):
-            return float(cur) / (total - 1)
+        scaling = np.array([
+            0., 0., 0., 0., 0., 0.,
+            .5, .5, .5, .5, .5, .5,
+            1., 1., 1., 1., 1., 1.,
+        ]).reshape((3, 2, 3))
+
         solid = gradients.init_solid(2, 3, (0, 0, 255))
-        result = gradients.gradient(solid, (0, 255, 0), linear)
+        result = gradients.gradient(solid, (0, 255, 0), scaling)
         expected = np.array([
             0, 0, 255, 0, 0, 255,
             0, 127.5, 127.5, 0, 127.5, 127.5,
@@ -26,10 +30,14 @@ class GradientsTest(unittest.TestCase):
         np.testing.assert_equal(result, expected)
 
     def test_quadratic_gradient(self):
-        def quadratic(cur, total):
-            return float(cur**2) / ((total - 1)**2)
+        scaling = np.array([
+            0., 0., 0., 0., 0., 0.,
+            .25, .25, .25, .25, .25, .25,
+            1., 1., 1., 1., 1., 1.,
+        ]).reshape((3, 2, 3))
+
         solid = gradients.init_solid(2, 3, (0, 0, 255))
-        result = gradients.gradient(solid, (0, 255, 0), quadratic)
+        result = gradients.gradient(solid, (0, 255, 0), scaling)
         expected = np.array([
             0, 0, 255, 0, 0, 255,
             0, 63.75, 191.25, 0, 63.75, 191.25,
